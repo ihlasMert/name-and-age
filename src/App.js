4473acf42257c1bcaad25b00ca712294d6a6bde4
ihlasMert <ihlas1mert@gmail.com>
2022-06-10 22:01:11 +0300

    created project and added re-usable button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import "./index.css";
 
-import './index.css';
-
-import AddUser from './components/Users/AddUser';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import { useState } from "react";
 
 function App() {
+  const [userstList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
-    <div >
-     <AddUser />
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={userstList} />
     </div>
   );
 }
